7ef3b3ab7ddc681a163df84435bc50587e0f2abb
gurk1sh <Gustaf_99@live.se>
2018-03-01 13:13:05 +0100

    asd

--- webbserver/index.php
+++ webbserver/index.php
@@ -22,7 +22,7 @@
 					<li><a href="index.php">Start</a></li>
 					<li><a href="sida2.php">Produkter</a></li>
 					<li><a href="sida3.php">Kundvagn</a></li>
-					<li><a href="#">Betalning</a></li>
+					<li><a href="#">BetalningTEST</a></li>
 				</ul>
 			</nav>
 			<main> <!--Huvudinnehåll-->
